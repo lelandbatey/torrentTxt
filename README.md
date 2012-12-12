1fbc479eb062f34a0fa099448ca5e3ad6c25306a
lelandbatey <lelandbatey@gmail.com>
2012-12-12 00:53:05 -0800

    Initial commit of torrentTxt

---
+++ README.md
@@ -0,0 +1,30 @@
+	   __                             __ ______     __ 
+	  / /_____  _____________  ____  / //_  __/  __/ /_
+	 / __/ __ \/ ___/ ___/ _ \/ __ \/ __// / | |/_/ __/
+	/ /_/ /_/ / /  / /  /  __/ / / / /_ / / _>  </ /_  
+	\__/\____/_/  /_/   \___/_/ /_/\__//_/ /_/|_|\__/  
+	                                                   
+Author: Leland Batey
+
+Description
+===========
+torrentTxt monitors a directory and whenever a file is added, you will receive a txt message (via Twilio) with the name of the file added. This makes a great addition to a seedbox that automatically adds torrents from an RSS feed, so you know exactly when you've got new downloads!
+
+Notes:
+------
+torrentTxt is meant to be installed as a cronJob that runs every few minutes.
+
+Requirements:
+-------------
+1. Python 2.7
+2. Twilio tools installed (`pip install Twilio`)
+3. A Twilio account
+
+Instructions:
+-------------
+1. Edit the torrentTxt.py file so that it has the correct settings for your needs. This means setting the appropriate directory to watch, your Twilio account SID and Auth Key, as well as the correct send and receive phone numbers.
+2. Set up a cron job to run the torrentTxt.py at a suitable interval. An example that runs every 5 minutes:
+		*/5 * * * * /path/to/python /path/to/torrentTxt.py
+	- Note that if you are using a virtualenvironment (something I highly recommend) then "/path/to/python" will be "/path/to/virtualenv/**bin/python**"
+3. ???
+4. Profit!
---
+++ torrentTxt.py
@@ -0,0 +1,85 @@
+"""
+   __                             __ ______     __ 
+  / /_____  _____________  ____  / //_  __/  __/ /_
+ / __/ __ \/ ___/ ___/ _ \/ __ \/ __// / | |/_/ __/
+/ /_/ /_/ / /  / /  /  __/ / / / /_ / / _>  </ /_  
+\__/\____/_/  /_/   \___/_/ /_/\__//_/ /_/|_|\__/  
+                                                   
+Author: Leland Batey
+
+Description
+-----------
+torrentTxt monitors a directory and whenever a file is added, you will recieve a txt message (via Twilio) with the name of the file added. torrentTxt is meant to be installed as a cronJob that runs every few minutes.
+
+Requirements:
+	Python 2.7
+	Twilio tools installed (pip install twilio)
+	A Twilio account
+
+"""
+
+from twilio.rest import TwilioRestClient
+import subprocess as sub
+import os
+
+#+-----+ Twilio Configuration Info +-----+
+# You're going to need to paste in your "Account SID" for "account", and your "Auth Token" for "token" 
+
+account = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+client = TwilioRestClient(account, token)
+fromNumber = "+xxxxxxxxxxx" # The number that registered to your Twilio account that you want to use to send texts from.
+toNumber = "+xxxxxxxxxxx" # The number you want to recieve text messages about the files added. This is probably your cell phone number.
+
+# Enter the full path to the directory that you want monitored
+searchDir = "XxXxXxX"
+trackingFile = "knownNames.txt" # This is the name of the file that stores the "known" list of files. You can change this name here.
+
+
+def mainHandler():
+	if os.path.isfile(os.getcwd()+"/"+trackingFile): # If the file tracking known filenames exists
+		knownNames = open(trackingFile,'r') 
+		knownNamesList = knownNames.read()
+		knownNames.close()
+		knownNamesList = knownNamesList.split('\n')
+	else: # If the tracking file doesn't exist, then fake it
+		knownNamesList = []
+
+	output = readDir()
+	splitLines = output.split('\n')
+	filenames = []
+	for stuff in splitLines:
+		filenames.append(stuff.split('/')[-1]) # Removes everything before the last forwardslash, leaving only each files name with no path.
+
+	diffList = list(set(filenames) - set(knownNamesList)) # Get the files that are not already in list of files we know about
+
+	if len(diffList) > 0:
+		buildString = "Files added:\n"
+
+		for stuff in diffList:
+			buildString += stuff+"\n"
+
+		buildString = buildString[:120] # Make the buildstring nice and short for use as a text message. It's assumed this will be with a trial account that puts a header on the top of the text message, so it has to accommadate the fewer characters 
+
+		print buildString
+
+		writeString = "" # This will build a string of all the known file names, with no asscociated folder
+		for stuff in filenames:
+			writeString += stuff+"\n"
+
+		# Write the new list of known files to the "knownFiles.txt" file
+		knownNames = open(trackingFile,'w')
+		knownNames.write(writeString)
+		knownNames.close()
+
+		client.sms.messages.create(to=toNumber, from_=fromNumber, body=buildString)
+
+def readDir(): # Gets the list of all files in the directory that is being monitored
+	searchCommand = "find ."
+	currentDirContents = sub.Popen(searchCommand,cwd=searchDir,stdout=sub.PIPE,stderr=sub.PIPE, shell=True)
+	output, errors = currentDirContents.communicate()
+
+	return output	
+
+# Since this is not currently meant to be a module, I'm skipping all the "__name__" stuff that normally goes in a python program.
+mainHandler()
